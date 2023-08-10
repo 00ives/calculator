26c90dc09556bffd48f67099b02bcc1cb5a41ab6
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2023-08-10 14:24:06 -0600

    file structure and styles

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberButtons =
+  document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll(
+  '[data-operation]'
+)
+const equalsButtons =
+  document.querySelectorAll('[data-equals]')
+const equalsButtons =
+  document.querySelectorAll('[data-equals]')
+const equalsButtons =
+  document.querySelectorAll('[data-equals]')
+const equalsButtons =
+  document.querySelectorAll('[data-equals]')
+const equalsButtons =
+  document.querySelectorAll('[data-equals]')
